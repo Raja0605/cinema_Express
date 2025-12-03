b49cccb055ae4d5973217a36e191fa5daeaf9b9a
Karuppu Raja <karuppuraja.s@primesoftinc.com>
2025-12-03 16:22:03 +0530

    Initial commit: Java project upload

--- src/main/java/ec/edu/ExpressCinemaApplication.java
+++ src/main/java/ec/edu/ExpressCinemaApplication.java
@@ -2,9 +2,10 @@ package ec.edu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ExpressCinemaApplication {
+public class ExpressCinemaApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ExpressCinemaApplication.class, args);
